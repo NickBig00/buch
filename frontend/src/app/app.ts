252885d581584d5b42ae5ff9c6049bb92cc67b7f
NickBig00 <gross.nick00@gmail.com>
2025-12-17 00:10:19 +0100

    intialized login in app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class App {}
