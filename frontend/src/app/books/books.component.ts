3e1303b4e864bfb7476c741d9468ac7af7a8e12a
NickBig00 <gross.nick00@gmail.com>
2025-12-19 15:22:36 +0100

    added books.components for navigation after login

---
+++ frontend/src/app/books/books.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-books',
+  template: `
+    <h1>Buchverwaltung</h1>
+    <p>Willkommen! Du bist eingeloggt.</p>
+  `,
+})
+export class BooksComponent {}
