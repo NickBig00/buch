9481c239f970d109ce94371286f5c70fe4bd9afd
NickBig00 <gross.nick00@gmail.com>
2026-01-02 14:21:32 +0100

    moved enviroment config to extra file

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://localhost:3000/rest',
+  keycloak: {
+    url: 'http://localhost:8880',
+    realm: 'nest',
+    clientId: 'buch-frontend',
+  },
+};
