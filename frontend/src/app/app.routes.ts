95f01d504fb390f47a48a493b6480c5851c5fbd1
NickBig00 <gross.nick00@gmail.com>
2026-01-04 17:01:30 +0100

    added route for new books

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { authGuard } from './auth/auth.guard';
 import { HomeComponent } from './home.component';
+import { BookCreateComponent } from './books/book-create.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -9,6 +10,8 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
 
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+
+  { path: 'books/new', component: BookCreateComponent, canActivate: [authGuard] },
 
   { path: '**', redirectTo: 'login' },
 ];
