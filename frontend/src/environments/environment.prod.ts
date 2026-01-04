9dacc7b30aab3be974245b974b29c4738de78d96
NickBig00 <gross.nick00@gmail.com>
2026-01-04 17:04:03 +0100

    corrected keyloak url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://localhost:3000/rest',
   keycloak: {
-    url: 'http://localhost:8880',
+    url: 'https://localhost:8843',
     realm: 'nest',
     clientId: 'buch-frontend',
   },
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://localhost:3000/rest',
   keycloak: {
-    url: 'http://localhost:8880',
+    url: 'https://localhost:8843',
     realm: 'nest',
     clientId: 'buch-frontend',
   },
